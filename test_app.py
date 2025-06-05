8d235c803ebcaf4295a5611199bd8defc6e9a751
Ronik3199 <ronik31@gmail.com>
2025-06-05 15:04:44 +0300

    initial commit

---
+++ test_app.py
@@ -0,0 +1,13 @@
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Welcome" in response.data
+
+def test_status():
+    client = app.test_client()
+    response = client.get('/status')
+    assert response.status_code == 200
+    assert response.json == {"status": "OK"}
