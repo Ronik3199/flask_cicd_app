0b15c5b135643393e5f69f88fd2997ab530203ef
Ronik3199 <ronik31@gmail.com>
2025-06-25 09:17:13 +0300

    Fix flake8 issues in app.py and test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,12 @@
 from app import app
 
+
 def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
     assert b"Welcome" in response.data
+
 
 def test_status():
     client = app.test_client()
